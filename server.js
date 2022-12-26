bd1788cd342a32df998ccfe0102d335d7b45b540
Gopal Meena <meenagopal24@gmail.com>
2022-12-26 16:18:38 +0530

    vercel json

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const port = 3000
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"templates/index.html"))
 })
-app.post('/merge', upload.array('pdfs',2), async (req,res,next)=>{
-    console.log(req.file);
-    let a = await mergerPdfs(path.join(__dirname,req.files[0].path), path.join(__dirname,req.files[1].path))
-    res.redirect(`http://localhost:3000/static/${a}.pdf`)
-})
+// app.post('/merge', upload.array('pdfs',2), async (req,res,next)=>{
+//     console.log(req.file);
+//     let a = await mergerPdfs(path.join(__dirname,req.files[0].path), path.join(__dirname,req.files[1].path))
+//     res.redirect(`http://localhost:3000/static/${a}.pdf`)
+// })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
